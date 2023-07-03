c7fadfc5d61769fd479e7ee1880daf74bfc51695
Md. Sazzad Hossain <96966545+Sazz07@users.noreply.github.com>
2023-07-04 00:01:17 +0600

    Done maximum part of client side UI

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import styles from './footer.module.css'
+import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div>
+        ©2023 Lamamia. All rights reserved.
+      </div>
+      <div className={styles.social}>
+          <Image src="/1.png" width={15} height={15} className={styles.icon} alt='Sazz dev Facebook'></Image>
+          <Image src="/2.png" width={15} height={15} className={styles.icon} alt='Sazz dev'></Image>
+          <Image src="/3.png" width={15} height={15} className={styles.icon} alt='Sazz dev'></Image>
+          <Image src="/4.png" width={15} height={15} className={styles.icon} alt='Sazz dev'></Image>
+      </div>
+    </div>
   )
 }
 
